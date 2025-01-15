062071f492597cc70e96c5c35b9448c9b6d191c3
Negar Ghorbani <negar.ghorbani77@gmail.com>
2025-01-15 22:18:04 +0100

    add backend API calls

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -2,7 +2,12 @@ import axios from "axios";
 
 const API_URL = "http://localhost:3000";
 
-export const fetchFundingRounds = async () => {
-  const response = await axios.get(`${API_URL}/funding-rounds`);
+export const fetchTop15FundingRounds = async () => {
+  const response = await axios.get(`${API_URL}/funding-rounds/top15`);
+  return response.data;
+};
+
+export const fetchFundingRoundsTimeline = async () => {
+  const response = await axios.get(`${API_URL}/funding-rounds/timeline`);
   return response.data;
 };
